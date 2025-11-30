1df57c25e5c551b6e95c0097b725237207fd3a01
sherwinspinto <sherwinspinto@gmail.com>
2025-11-29 17:44:40 -0800

    Created new project "Conference Ticket Booking System Evolution" 1. Use https://start.spring.io/ to create the project. 2. Include Spring Web dependency. 3. Include Spring JDBC dependency. 4. Added gitignore

---
+++ src/main/java/com/ticket_booking_service_evolution/ticket_booking_service_evolution/TicketBookingServiceEvolutionApplication.java
@@ -0,0 +1,13 @@
+package com.ticket_booking_service_evolution.ticket_booking_service_evolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingServiceEvolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingServiceEvolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticket_booking_service_evolution/ticket_booking_service_evolution/TicketBookingServiceEvolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket_booking_service_evolution.ticket_booking_service_evolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketBookingServiceEvolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
