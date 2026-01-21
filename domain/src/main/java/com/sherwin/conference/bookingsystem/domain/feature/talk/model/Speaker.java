dd6a261cfab5a4ba85c80362d69cd354fd8eb1e2
Sherwin Pinto <sherwinspinto@gmail.com>
2026-01-20 22:33:35 -0800

    chore(bookingsystem): Separate monolith into modules (conf-106 #15) (#21)
    Separate the booking system monolith into 3 modules
    domain, infrastructure and launcher, following hexagonal
    architecture

---
+++ domain/src/main/java/com/sherwin/conference/bookingsystem/domain/feature/talk/model/Speaker.java
@@ -0,0 +1,32 @@
+package com.sherwin.conference.bookingsystem.domain.feature.talk.model;
+
+import static com.sherwin.conference.bookingsystem.domain.feature.commons.validations.Utils.collect;
+import static com.sherwin.conference.bookingsystem.domain.feature.model.MetaData.*;
+
+import com.sherwin.conference.bookingsystem.domain.feature.commons.validations.FieldError;
+import com.sherwin.conference.bookingsystem.domain.feature.commons.validations.FieldError.ErrorParams.FieldValue;
+import com.sherwin.conference.bookingsystem.domain.feature.model.CreationResult;
+import com.sherwin.conference.bookingsystem.domain.feature.talk.model.TalkFieldType.FirstName;
+import com.sherwin.conference.bookingsystem.domain.feature.talk.model.TalkFieldType.LastName;
+import com.sherwin.conference.bookingsystem.domain.feature.commons.validations.CommonValidations;
+import java.util.List;
+import java.util.Optional;
+
+public record Speaker(FirstName firstName, LastName lastName) {
+  public static CreationResult<Speaker> of(
+      String firstName, String lastName, PathProvider context) {
+    List<FieldError> errors = collect(validate(firstName, lastName, context));
+    return errors.isEmpty()
+        ? CreationResult.success(new Speaker(new FirstName(firstName), new LastName(lastName)))
+        : CreationResult.failure(errors);
+  }
+
+  public static List<Optional<FieldError>> validate(
+      String firstName, String lastName, PathProvider context) {
+    return List.of(
+        CommonValidations.validateEmptyString(
+            TalkMetaData.Speaker.FIRST_NAME.at(context).toFieldName(), FieldValue.of(firstName)),
+        CommonValidations.validateEmptyString(
+            TalkMetaData.Speaker.LAST_NAME.at(context).toFieldName(), FieldValue.of(lastName)));
+  }
+}
---
+++ infrastructure/src/main/java/com/sherwin/conference/bookingsystem/infrastructure/controller/talk/GetTalksController.java
@@ -0,0 +1,31 @@
+package com.sherwin.conference.bookingsystem.infrastructure.controller.talk;
+
+import com.sherwin.conference.bookingsystem.domain.feature.model.PageRequest;
+import com.sherwin.conference.bookingsystem.domain.feature.talk.api.port.GetTalksPort;
+import com.sherwin.conference.bookingsystem.infrastructure.dto.talk.TalkDto;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/talks")
+public class GetTalksController {
+  private final GetTalksPort getTalksPort;
+
+  public GetTalksController(GetTalksPort getTalksPort) {
+    this.getTalksPort = getTalksPort;
+  }
+
+  @GetMapping("/")
+  public ResponseEntity<List<TalkDto>> getAllTalks(Pageable pageable) {
+    return ResponseEntity.ok(getTalksPort.getAllTalks(toDomain(pageable)).stream().map(TalkDto::fromDomain).toList());
+  }
+
+  private PageRequest toDomain(Pageable pageable) {
+    return PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+  }
+}
